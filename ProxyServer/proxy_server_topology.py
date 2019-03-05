34a8f2ff04e3773d6de15406d48368f7025ef99c
spartakos87 <serepasf@yahoo.gr>
2019-03-05 14:12:49 +0200

    update python file

--- ProxyServer/proxy_server_topology.py
+++ ProxyServer/proxy_server_topology.py
@@ -9,17 +9,30 @@ from mininet.log import setLogLevel
 
 class SingleSwitchTopo(Topo):
     "Single switch connected to n hosts."
-    def build(self, n=2):
+    def build(self):
+        """
+
+        :return:
+        """
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+    # def build(self, n=2):
         switch = self.addSwitch('s1')
-        # Python's range(N) generates 0..N-1
-        for h in range(n):
-            host = self.addHost('h%s' % (h + 1))
-            self.addLink(host, switch)
+        host1 = self.addHost('h1',
+                            defaultRoute='via 172.17.0.1:3128')
+        self.addLink(host1, switch)
+        host2 = self.addHost('h2')
+        self.addLink(host2, switch)
+        # # Python's range(N) generates 0..N-1
+        # for h in range(n):
+        #     host = self.addHost('h%s' % (h + 1))
+        #     self.addLink(host, switch)
 
 
 def simpleTest():
     "Create and test a simple network"
-    topo = SingleSwitchTopo(n=10000)
+    # topo = SingleSwitchTopo(n=10000)
+    topo = SingleSwitchTopo()
     net = Mininet(topo)
     net.start()
     print "Dumping host connections"
