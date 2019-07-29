546c69633ee6dec48629ba8cd09549cab5195809
spartakos87 <serepasf@yahoo.gr>
2019-07-29 18:28:04 +0300

    update toplogy add two servers

--- powderwireless/proxy.py
+++ powderwireless/proxy.py
@@ -19,7 +19,10 @@ switch_2 = request.RawPC("switch2")
 controller = request.RawPC("controller")
 proxy_server_1 = request.RawPC("proxy_server1")
 proxy_server_2 = request.RawPC("proxy_server2")
-# server = request.RawPC("server")
+
+# Set up servers which clients will ping
+server_1 = request.RawPC("server1")
+server_2 = request.RawPC("server2")
 
 # Install Squid in proxy_server
 install_squid = "sudo apt-get --assume-yes install squid"
@@ -27,10 +30,6 @@ update = "sudo apt-get update"
 proxy_server_1.addService(rspec.Execute(shell="bash", command=update+"; "+install_squid))
 proxy_server_2.addService(rspec.Execute(shell="bash", command=update+"; "+install_squid))
 
-# Install ovs in switches
-ovs_install = "sudo apt-get --assume-yes install openvswitch-switch"
-switch_1.addService(rspec.Execute(shell="bash", command=update+"&& "+ovs_install))
-switch_2.addService(rspec.Execute(shell="bash", command=update+"&& "+ovs_install))
 
 # Install POX in controller node 
 
@@ -38,14 +37,24 @@ cmd = "cd /home && sudo mkdir POX && sudo  git clone http://github.com/noxrepo/p
 controller.addService(rspec.Execute(shell="bash", command=cmd))
 
 
-link1 = request.Link(members = [switch_1, controller])
-link2 = request.Link(members = [switch_2, controller])
-link3 = request.Link(members = [client_1, switch_1])
-link4 = request.Link(members = [client_2, switch_1])
-link5 = request.Link(members = [client_3, switch_2])
-link6 = request.Link(members = [client_4, switch_2])
-link7 = request.Link(members = [proxy_server_2, switch_2])
-link8 = request.Link(members = [proxy_server_1, switch_1])
+link1 = request.Link(members=[switch_1, controller])
+link2 = request.Link(members=[switch_2, controller])
+link3 = request.Link(members=[client_1, switch_1])
+link4 = request.Link(members=[client_2, switch_1])
+link5 = request.Link(members=[client_3, switch_2])
+link6 = request.Link(members=[client_4, switch_2])
+link7 = request.Link(members=[proxy_server_2, switch_2])
+link8 = request.Link(members=[proxy_server_1, switch_1])
+
+# Switches and Proxy serves will be lingked with servers
+link9 = request.Link(members=[proxy_server_1, server_1])
+link10 = request.Link(members=[proxy_server_1, server_2])
+
+# After linked
+# Install ovs in switches
+ovs_install = "sudo apt-get --assume-yes install openvswitch-switch"
+switch_1.addService(rspec.Execute(shell="bash", command=update+"&& "+ovs_install))
+switch_2.addService(rspec.Execute(shell="bash", command=update+"&& "+ovs_install))
 
 
 portal.context.printRequestRSpec()
