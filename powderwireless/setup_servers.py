8d77c17289d5a792f179f79e8118f048451313d8
spartakos87 <serepasf@yahoo.gr>
2019-09-15 23:51:15 +0300

    use python3 and replace the SimpleHttpServer with http.server

--- powderwireless/setup_servers.py
+++ powderwireless/setup_servers.py
@@ -4,12 +4,12 @@ if __name__ == '__main__':
     link_img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-67FFQJYGDA0mklORazEaP1O1AulxO816mwfJaEm69A0BgH6ShQ"
     # Download the given jpg, which is located in link_img url and rename it to download.jpg
     get_it = "wget -cO - "+link_img+" > download.jpg"
-    create_folder = "mkdir foo"
-    mv_downimg_tofoo = "mv download.jpg foo/"
-    cd_folder = "cd foo"
-    run_server = "python -m SimpleHTTPServer 1313"
+    # create_folder = "mkdir foo"
+    # mv_downimg_tofoo = "mv download.jpg foo/"
+    # cd_folder = "cd foo"
+    run_server = "python3 -m http.server 1313"
     os.system(get_it)
-    os.system(create_folder)
-    os.system(mv_downimg_tofoo)
-    os.system(cd_folder)
+    # os.system(create_folder)
+    # os.system(mv_downimg_tofoo)
+    # os.system(cd_folder)
     os.system(run_server)
